10bd2b2b5c91bd3d06c238473d90ee010a312134
hwi215 <buc06137@naver.com>
2023-03-20 14:51:28 +0900

    add [boj] DP 문제풀이

--- DP/[BaekJoon] 11052번_카드구매.py
+++ DP/[BaekJoon] 11052번_카드구매.py
@@ -1,4 +1,5 @@
 # 개미 전사와 같은 문제
+'''
 n = int(input())
 # 비용
 cost = list(map(int, input().split()))
@@ -17,3 +18,14 @@ for i in range(1, n+1): # 1부터 n까지
 
 print(dp[n])
 
+'''
+n = int(input())
+li = [0] + list(map(int, input().split()))
+
+dp = [0 for _ in range(n+1)]
+
+for i in range(1, n+1):
+    for k in range(1, i+1):
+        dp[i] = max(dp[i], dp[i-k] + li[k])
+
+print(dp[n])
