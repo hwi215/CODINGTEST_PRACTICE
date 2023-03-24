66fbc32c9224b743178e3d0b1066fe894b4370c9
hwi215 <buc06137@naver.com>
2023-03-24 16:58:26 +0900

    add [백준] sam코테 대비 5일차

---
+++ 삼성SW/boj_14889_스타트와링크.py
@@ -0,0 +1,41 @@
+# 목표) 두 팀 점수의 차이 최소값 찾기
+# dfs(백트래킹)
+
+import sys
+input = sys.stdin.readline
+
+graph = []
+n = int(input())
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
+
+visited = [False for _ in range(n)]
+result = int(1e9)
+
+def dfs(depth, index):
+    global result
+
+    # 중단 조건
+    if n//2 == depth:
+        sum1, sum2 = 0, 0
+        for i in range(n):
+            for j in range(i+1, n):
+                if visited[i] and visited[j]:
+                    sum1 += graph[i][j]
+                    sum1 += graph[j][i]
+                elif not visited[i] and not visited[j]:
+                    sum2 += graph[i][j]
+                    sum2 += graph[j][i]
+
+        result = min(result, abs(sum1-sum2))
+        return
+
+    # dfs
+    for i in range(index, n):
+        if not visited[i]:
+            visited[i] = True # 방문체크
+            dfs(depth+1, i+1)
+            visited[i] = False
+
+dfs(0, 0)
+print(result)
